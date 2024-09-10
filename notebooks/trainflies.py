b61771c409e87a3f332198578d2d07a066677e12
Yinshu (Salina) Cai <cselina0258@gmail.com>
2024-09-10 14:29:08 -0400

    Update trainflies.py

--- notebooks/trainflies.py
+++ notebooks/trainflies.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('home/ycai223/code/rnn_toolkit/')
 
-from rnn_toolkit.data import preprocessing
+from rnn_toolkit.models import LSTMModel
 
 print('import successful')
 #read data
